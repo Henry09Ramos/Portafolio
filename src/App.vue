50df10037c5b6a753ded168552319bb7d949ba67
HenryRamos <henryjeremiasramos09@gmail.com>
2023-11-07 14:26:30 -0600

    Portafolio

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+   <Portafolio/>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Portafolio from './components/Portafolio.vue'
+</script>
+
---
+++ src/components/Portafolio.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class=" bg-indigo-100 p-4 md:p-8 lg:p-10 lg:px-40  ">
+    <h4 class="text-sky-700 text-2xl md:text-3xl text-center mb-4">
+      <b>Hello👋, welcome, I'm Henry Ramos👨🏻‍💻</b>
+    </h4>
+
+    <hr class="border-t border-blue-700 my-4">
+
+    <div class="flex flex-col md:flex-row items-center">
+      <img
+        class="object-cover rounded-full w-48 h-48 md:w-48 md:h-48 mb-4 md:mb-0"
+        src="https://camo.githubusercontent.com/98cce6a9aef4a0abc949c11e239c75d327068570a550265fc180da96e522ba20/68747470733a2f2f692e70696e696d672e636f6d2f353634782f39612f34302f36362f39613430363639353237623236303637613339326264306464343535343238352e6a7067"
+        alt="Profile Image"
+      />
+
+      <p class="text-sm text-center md:text-left px-6">
+        <b class="italic text-black">🗒️Information</b>
+        <br />
+        I am a student of the Software Development Technician program.
+         I am an intern at SeedCode Sv, where I am gaining top-quality 
+         experience, learning with attitudes and values to manage our clients' 
+         needs with the best quality and commitment in the development of their projects. 
+         I am in both the Frontend and Backend areas, improving my skills and working on enhancing 
+         my weaknesses to become a reliable and committed team member.
+      </p>
+    </div>
+<br>
+<br>
+
+    <div>
+      <b class="italic">🔧⚙️Technologies & Tools</b>
+      <br>
+      <div class="flex flex-wrap items-center justify-center md:justify-start">
+        <img
+          class="p-2  md:w-auto md:h-auto "
+          src="https://img.shields.io/badge/Visual_Studio-5C2D91?style=for-the-badge&logo=visual%20studio&logoColor=white"
+               />
+               
+          <img
+          class="p-2 md:w-auto md:h-auto "
+          src="https://img.shields.io/badge/Visual_Studio_Code-0078D4?style=for-the-badge&logo=visual%20studio%20code&logoColor=white"
+              />
+        <img
+          class="p-2 md:w-auto md:h-auto "
+          src="https://img.shields.io/badge/C%23-239120?style=for-the-badge&logo=c-sharp&logoColor=white"
+            /><img
+          class="p-2 md:w-auto md:h-auto "
+          src="https://img.shields.io/badge/Xamarin-3498DB?style=for-the-badge&logo=xamarin&logoColor=white"
+            />
+            <img
+          class="p-2 md:w-auto md:h-auto "
+          src="https://img.shields.io/badge/.NET-5C2D91?style=for-the-badge&logo=.net&logoColor=white"
+            />
+            <img
+          class="p-2 md:w-auto md:h-auto "
+          src="https://img.shields.io/badge/Vue.js-35495E?style=for-the-badge&logo=vue.js&logoColor=4FC08D"        
+             />
+             <img
+          class="p-2 md:w-auto md:h-auto "
+          src="https://img.shields.io/badge/GIT-E44C30?style=for-the-badge&logo=git&logoColor=white"        
+             />
+             <img
+          class="p-2 md:w-auto md:h-auto "
+          src="https://img.shields.io/badge/Canva-%2300C4CC.svg?&style=for-the-badge&logo=Canva&logoColor=white"        
+             />
+             <img
+          class="p-2 md:w-auto md:h-auto "
+          src="https://img.shields.io/badge/Azure_DevOps-0078D7?style=for-the-badge&logo=azure-devops&logoColor=white "        
+             />
+             <img
+          class="p-2 md:w-auto md:h-auto "
+          src=" https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white"        
+             />
+           
+           
+      
+      </div>
+    </div>
+
+    <hr class="border-t border-blue-700 my-4">
+
+    <div>
+      <b class="italic">📝Currently in learning</b>
+      <br>
+      <p class="text-sm text-center md:text-left px-6">
+        I'm on the path of learning, in which I'm currently 
+        learning new programming languages, as well as learning 
+        a CSS framework, which is very fundamental for me, as a 
+        programmer, of which they are :
+       </p>
+       <br>
+      <div class="flex flex-wrap items-center justify-center md:justify-start">
+        
+          <img
+          class="p-2 md:w-auto md:h-auto "
+          src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" />
+          <img
+          class="p-2 md:w-auto md:h-auto "
+           src="https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white" />
+          <img
+          class="p-2 md:w-auto md:h-auto "
+          src="https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white"/>
+
+      </div >
+      <hr class="border-t border-blue-700 my-4">
+      <div class=" flex-wrap items-center justify-center md:justify-start">
+        <b class="italic ">💻📱Social Media</b>
+        <br>
+        <br>
+       <a href="https://www.linkedin.com/in/henry-jerem%C3%ADas-ramos-p%C3%A9rez-a6b74b295/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noopener noreferrer">
+      <img 
+      class="ml-[20px] md:w-auto md:h-auto "
+      src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white"
+      alt="LinkedIn"
+      >
+    </a> 
+          <br>
+         
+ 
+      </div>
+    </div>
+  </div>
+</template>
